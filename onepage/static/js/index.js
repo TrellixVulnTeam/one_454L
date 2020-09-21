6c0b09005dde73c2304039f4b8e36c97677cdcb4
sixtysixcode <alexbarnes000@gmail.com>
2020-09-21 14:30:06 +0100

    🌏 Added animation to navigation and linkedin icon

--- onepage/static/js/index.js
+++ onepage/static/js/index.js
@@ -1,7 +1,18 @@
 window.onload = function() {
-    var menu = document.getElementById('menu');
-    var hamburger = document.getElementById('hamburger');
+    let menu = document.getElementById('menu');
+    let hamburger = document.getElementById('hamburger');
+    let menu_item = document.getElementsByClassName('nav__desktop__items__item');
+    let mobile_menu_item = document.getElementsByClassName('nav__mobile__items__item');
+
     hamburger.addEventListener('click', navStatus);
+
+     for (let i = 0; i < menu_item.length; i++) {
+        menu_item[i].addEventListener('click', navUnderline);
+    }
+
+     for (let i = 0; i < mobile_menu_item.length; i++) {
+         mobile_menu_item[i].addEventListener('click', mobileNavUnderline);
+     }
 
     function navStatus() {
         if ((hamburger).classList.contains('nav__mobile__hamburger-icon-container-active')) {
@@ -20,4 +31,14 @@ window.onload = function() {
         (hamburger).classList.add('nav__mobile__hamburger-icon-container-active');
         (menu).classList.add('nav__mobile__items-is-open');
     }
-}
+
+    function navUnderline() {
+            $(menu_item).removeClass('active');
+            $(this).addClass('active');
+        }
+
+     function mobileNavUnderline() {
+         $(mobile_menu_item).removeClass('active');
+         $(this).addClass('active');
+     }
+};
